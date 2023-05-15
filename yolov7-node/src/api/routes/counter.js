04152c70c9ccffcbd49b6a5fc9201b5243f39244
kerem-ozt <mkerem@marun.edu.tr>
2023-05-15 08:55:25 +0300

    added database post services

--- yolov7-node/src/api/routes/counter.js
+++ yolov7-node/src/api/routes/counter.js
@@ -5,5 +5,8 @@ const CounterRouter = express.Router();
 
 CounterRouter.get('/run', CounterController.runDetector);
 CounterRouter.get('/runsave', CounterController.runDetectorSaveData);
+CounterRouter.get('/test', CounterController.test);
+CounterRouter.get('/normaltest', CounterController.testWithNormalDatabase);
+CounterRouter.get('/son', CounterController.son);
 
 export default CounterRouter;
--- yolov7-node/src/api/services/counter.js
+++ yolov7-node/src/api/services/counter.js
@@ -2,6 +2,7 @@ import { Server } from 'socket.io'
 import server from '../../index.js';
 import { OnnxKlas } from "../../utils/onnxcore.js";
 import { getDatabase } from 'firebase-admin/database';
+import admin from 'firebase-admin';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7eJA-Gl-Yi86KDX8nxf_67yTm3Ovd-M8",
@@ -58,25 +59,171 @@ class CounterService {
 
     static async runDetectorSaveData (req) {
         try{
+            const db = admin.firestore();
+            
+            let customerRef = db.collection('people');
+
+            customerRef.get().then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    console.log(doc.id, '=>', doc.data());
+                });
+            })
+            
+            return {type: true, message: 'Data saved successfully'};
+        }
+        catch (err) {
+            console.log('a',err);
+            return {type: false, message: err};
+        }
+    }
+
+    static async test (req) {
+        try{
+
             const db = getDatabase();
         
             const ref = db.ref();
             const usersRef = ref.child('users');    
 
-            usersRef.set({
-            alanisawesome: {
-                date_of_birth: 'June 23, 1912',
-                full_name: 'Alan Turing'
-            },
-            gracehop: {
-                date_of_birth: 'December 9, 1906',
-                full_name: 'Grace Hopper'
+            const modelInfo = {
+                name: 'yolov7-tiny.onnx', // ONNX dosyası
+                inputShape: [1, 3, 640, 640] // model için giriş tensörünün şekli
             }
-            });
-            return {type: true, message: 'Data saved successfully'};
+
+            const io = new Server(server, {
+                maxHttpBufferSize: 1e8,
+                pingTimeout: 60000,
+                cors: {
+                    origin: 'http://localhost:3000', 
+                    methods: ['GET', 'POST'] 
+                }
+            })
+
+            io.on('connection', (socket) => {
+                // model bilgilerini istemciye gönder
+                socket.emit('model-env', modelInfo)
+
+                socket.on('disconnect', () => {
+                    // kullanıcı soket bağlantısının kesildiğini bildirir
+                    console.log('user disconnected')
+                })
+
+                // önyüzden görüntü dizisi alır ve görüntüdeki nesneyi algılamak üzere detect fonskiyonunu çalıştırır
+                socket.on('videoframe', async (frame, xRatio, yRatio, callback) => {
+                    const boxes = await OnnxKlas.detect(frame, xRatio, yRatio)
+                    // algılama sonuçlarını önyüze geri gönder
+                    callback(boxes)
+                    console.log(boxes);
+                    usersRef.set({
+                        test: {
+                            date_of_birth: new Date(),
+                            full_name: boxes
+                        },
+                    });
+                })
+            })
+
         }
         catch (err) {
             console.log('a',err);
+            return {type: false, message: err};
+        }
+    }
+
+    static async testWithNormalDatabase (req) {
+        try{
+            const db = admin.firestore();
+            
+            let customerRef = db.collection('people');
+
+            // db.collection('people').doc('peoplecount').set({
+            //     people: 4
+            // });
+
+            customerRef.add({
+                people: 5
+            });
+
+            customerRef.get().then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    console.log(doc.id, '=>', doc.data());
+                });
+            })
+
+            // const cityRef = db.collection('people').doc('peoplecount');
+
+            // await cityRef.set({
+            //     title: new Date(),
+            //     body: 'Hello World',
+            // });
+
+            // await cityRef.set({
+            //     title: new Date(),
+            //     body: 'Hello World',
+            // });
+
+            // const res = await cityRef.add({
+            //     people: 4
+            // });
+
+            // const res = await cityRef.set({
+            //     capital: true
+            // }, { merge: true });
+
+            return {type: true, message: 'Data saved successfully'};
+        }
+        catch (err) {
+            console.log('1',err);
+            return {type: false, message: err};
+        }
+    }
+
+    static async son (req) {
+        try{
+
+            const db = admin.firestore();
+            
+            let customerRef = db.collection('people');
+
+            const modelInfo = {
+                name: 'yolov7-tiny.onnx', // ONNX dosyası
+                inputShape: [1, 3, 640, 640] // model için giriş tensörünün şekli
+            }
+
+            const io = new Server(server, {
+                maxHttpBufferSize: 1e8,
+                pingTimeout: 60000,
+                cors: {
+                    origin: 'http://localhost:3000', 
+                    methods: ['GET', 'POST'] 
+                }
+            })
+
+            io.on('connection', (socket) => {
+                // model bilgilerini istemciye gönder
+                socket.emit('model-env', modelInfo)
+
+                socket.on('disconnect', () => {
+                    // kullanıcı soket bağlantısının kesildiğini bildirir
+                    console.log('user disconnected')
+                })
+
+                // önyüzden görüntü dizisi alır ve görüntüdeki nesneyi algılamak üzere detect fonskiyonunu çalıştırır
+                socket.on('videoframe', async (frame, xRatio, yRatio, callback) => {
+                    const boxes = await OnnxKlas.detect(frame, xRatio, yRatio)
+                    // algılama sonuçlarını önyüze geri gönder
+                    callback(boxes)
+                    console.log(boxes);
+                    customerRef.add({
+                        date: new Date(),
+                        box: boxes
+                    });
+                })
+            })
+
+        }
+        catch (err) {
+            console.log('1',err);
             return {type: false, message: err};
         }
     }
